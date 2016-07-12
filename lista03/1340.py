786dc2682c206d0edc641cca2122bcaf55712477
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-07-12 20:16:44 -0300

    1340 feita

--- lista03/1340.py
+++ lista03/1340.py
@@ -1,41 +1,12 @@
 #coding: utf-8
 
-def queue(inp, out):
-	cont = 0
-	for i in xrange(len(out)):
-		if inp[i] == out[i]:
-			cont += 1
-	if cont > 0: return True
-	return False
-
-def stack(inp, out):
-	j = 0
-	cont = 0
-	for i in xrange(len(out)):
-		j -=1
-		if inp[j] == out[i]:
-			cont += 1
-	if cont > 0: return True
-	return False
-
-def priorityQueue(inp, out):
-	cont = 0
-	for i in xrange(len(out)):
-		if out[i] == max(inp):
-			cont += 1
-			inp.remove(max(inp))
-	if cont > 0: return True
-	return False
-
-
 #######MAIN
-inp = []
-out = []
-saida = []
-a = 0
-queue = 0
-stack = 0
-priority = 0
+p = True
+q = True
+s = True
+queue = []
+stack = []
+priority = []
 while True:
 	try:
 		n = int(raw_input())
@@ -43,26 +14,30 @@ while True:
 	except EOFError:
 		break 
 	
+	queue, stack, priority = [],[],[]
+	p, q, s = True, True, True
 	for i in range(n):
+		
+		
 		entrada = raw_input().split()
-		if int(entrada[0]) == 1: inp.append(int(entrada[1]))
-		elif int(entrada[0]) == 2 and a >= 1:
-			a = 0
-			continue
-		else: 
-			out.append(int(entrada[1]))
-			if queue(inp, out): saida.append("queue")
-			if stack(inp, out): saida.append("stack")
-			if priorityQueue(inp, out): saida.append("priority queue")
-			a += 1
+		if int(entrada[0]) == 1: 
+			queue.append(int(entrada[1]))
+			stack.insert(0,int(entrada[1]))
+			priority.append(int(entrada[1]))
 			
-	
-	
-	if len(saida) == 0: print "impossible"
-	elif len(saida) == 1: print saida[0]
-	else: print "not sure"
-	print saida
-	
-	inp, out, saida = [],[],[]
+		else: 
+			if queue[0] != int(entrada[1]): q = False
+			if stack[0] != int(entrada[1]): s = False
+			if max(priority) != int(entrada[1]): p = False
+			queue.pop(0)
+			stack.pop(0)
+			priority.remove(max(priority))
 
-	 
+	if (p and q and s) or (not p and q and s) or (p and not q and s) or (p and q and not s):
+		print "not sure"
+	elif (p and not q and not s): print "priority queue"
+	elif (not p and not q and s): print "stack"
+	elif (not p and q and not s): print "queue"
+	else: print "impossible"
+	
+	
